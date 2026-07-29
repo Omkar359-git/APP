7e66c441248cae55d0c00d688f46939fdb3ee722
Omkar359-git <omkarsolanke35@gmail.com>
2026-07-29 17:28:53 +0530

    Add files via upload

---
+++ basic OPP.py
@@ -0,0 +1,18 @@
+class Student:
+    
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+    def display(self):
+        print("Name:", self.name)
+        print("Roll No:", self.roll_no)
+
+
+s1 = Student("Omkar", 101)
+s1.display()
+
+'''
+Name: Omkar
+Roll No: 101
+'''
